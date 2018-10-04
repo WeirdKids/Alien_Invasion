3393387da20e8da8b1253042d6628cae4b4b92d1
WiredKid <18742585882@163.com>
2018-10-04 21:48:21 +0800

    创建外星人群

--- alien_invasion.py
+++ alien_invasion.py
@@ -20,8 +20,11 @@ def run_game():
     #创建一个用于存储子弹的编组
     bullets = Group()
 
-    #创建一个外星人
-    alien = Alien(ai_settings, screen)
+    #创建一个外星人编组
+    aliens = Group()
+
+    #创建外星人群
+    gf.create_fleet(ai_settings, screen, aliens)
 
     #开始游戏的主循环
     while True:
@@ -29,6 +32,6 @@ def run_game():
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, alien, bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
         
 run_game()
