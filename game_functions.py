c36b07b5c43598a20ff69654be43c36a1fdb89be
WiredKid <18742585882@163.com>
2018-10-04 19:32:15 +0800

    封装发射子弹函数

--- game_functions.py
+++ game_functions.py
@@ -13,10 +13,14 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
     #向左移动飞船
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
+        fire_bullet(ai_settings, screen, ship, bullets)
+
+def fire_bullet(ai_settings, screen, ship, bullets):
+    """如果还没有到达限制，就发射一枚子弹"""
     #创建一颗子弹，并将其加入到编组bullets中
-        if len(bullets) < ai_settings.bullets_allowed:
-            new_bullet = Bullet(ai_settings, screen, ship)
-            bullets.add(new_bullet)
+    if len(bullets) < ai_settings.bullets_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
 
 def check_keyup_events(event, ship):
     """响应松开"""
